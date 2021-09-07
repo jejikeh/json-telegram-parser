cb6d8779589b3d6680e86aa5e4995cddbf2ccde6
evgnidium <1cqxaz1cqxaz@gmail.com>
2021-09-07 21:35:59 +0300

    fix

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const { randomInt } = require("crypto");
 var fs = require("fs")
-var numChat = 0
+var numChat = 1
 fs.readFile('result.json',
 
     function(err,data){
         var jsonData = data;
         var jsnoParsed = JSON.parse(jsonData);
         for(var i = 0; i < 18201; i++){
-            if(jsnoParsed.chats.list[numChat].messages[i].text == "" && jsnoParsed.chats.list[numChat].messages[i].sticker_emoji != null ){
-                console.log(jsnoParsed.chats.list[numChat].messages[i].sticker_emoji)
-            }else if(jsnoParsed.chats.list[numChat].messages[i].text != null){
-                console.log(jsnoParsed.chats.list[numChat].messages[i].text)
+            if(jsnoParsed.chats.list[numChat].messages[i].type == "message"){
+                if(jsnoParsed.chats.list[numChat].messages[i].text == "" && jsnoParsed.chats.list[numChat].messages[i].sticker_emoji != null ){
+                    console.log(jsnoParsed.chats.list[numChat].messages[i].sticker_emoji)
+                }else if(jsnoParsed.chats.list[numChat].messages[i].text != null){
+                    console.log(jsnoParsed.chats.list[numChat].messages[i].text)
+                }
+            }else {
+                return 0;
             }
         }
     }
