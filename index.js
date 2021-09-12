48aac01232b4f48f23ddda4c0329a7d13cc7fcd1
evgnidium <1cqxaz1cqxaz@gmail.com>
2021-09-13 00:32:24 +0300

    13 sep 2021

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ fs.readFile('result.json',
         var jsonParsed = JSON.parse(jsonData);
         for(var i = 0; i < Object.keys(jsonParsed.chats.list[numChat].messages).length; i++){
             if(jsonParsed.chats.list[numChat].messages[i].text == "" && jsonParsed.chats.list[numChat].messages[i].sticker_emoji != null && !Array.isArray(jsonParsed.chats.list[numChat].messages[i].text) ){
-                console.log(jsonParsed.chats.list[numChat].messages[i].sticker_emoji)
                 fs.appendFileSync('messages.txt',jsonParsed.chats.list[numChat].messages[i].sticker_emoji, function (err){
                     if (err) {
                         console.log(err);
@@ -20,7 +19,6 @@ fs.readFile('result.json',
                 if(jsonParsed.chats.list[numChat].messages[i].text.text != null){
                     return 0;
                 }else {
-                    console.log(jsonParsed.chats.list[numChat].messages[i].text)
                     fs.appendFileSync('messages.txt',toString(jsonParsed.chats.list[numChat].messages[i].text), function (err){
                         if (err) {
                             console.log(err);
@@ -30,9 +28,7 @@ fs.readFile('result.json',
                     
                 })
         }
-        }else if(jsonParsed.chats.list[numChat].messages[i].text == 'https://youtu.be/8QWrDxXFIc8'){
-            console.log("dd")
-        }
+    }
     }
 }
 
