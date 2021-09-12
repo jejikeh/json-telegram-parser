5d96e5b4aad7379c6074acc556bd77b043ba86a6
evgnidium <1cqxaz1cqxaz@gmail.com>
2021-09-13 00:04:57 +0300

    12:04 13 sep 2021

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const { randomInt } = require("crypto");
 var fs = require("fs")
-var numChat = 1
+var numChat = 0
 fs.readFile('result.json',
 
     function(err,data){
         var jsonData = data;
-        var jsnoParsed = JSON.parse(jsonData);
-        for(var i = 0; i < 18201; i++){
-            if(jsnoParsed.chats.list[numChat].messages[i].type == "message"){
-                if(jsnoParsed.chats.list[numChat].messages[i].text == "" && jsnoParsed.chats.list[numChat].messages[i].sticker_emoji != null ){
-                    console.log(jsnoParsed.chats.list[numChat].messages[i].sticker_emoji)
-                }else if(jsnoParsed.chats.list[numChat].messages[i].text != null){
-                    console.log(jsnoParsed.chats.list[numChat].messages[i].text)
-                }
-            }else {
-                return 0;
-            }
+        var jsonParsed = JSON.parse(jsonData);
+        for(var i = 0; i < Object.keys(jsonParsed.chats.list[numChat].messages).length; i++){
+            if(jsonParsed.chats.list[numChat].messages[i].text == "" && jsonParsed.chats.list[numChat].messages[i].sticker_emoji != null && !Array.isArray(jsonParsed.chats.list[numChat].messages[i].text) ){
+                console.log(jsonParsed.chats.list[numChat].messages[i].sticker_emoji)
+                fs.appendFileSync('messages.txt',jsonParsed.chats.list[numChat].messages[i].sticker_emoji, function (err){
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("cemf");
+                    }
+                })
+            }else if(jsonParsed.chats.list[numChat].messages[i].text != null){
+                if(jsonParsed.chats.list[numChat].messages[i].text.text != null){
+                    return 0;
+                }else {
+                    console.log(jsonParsed.chats.list[numChat].messages[i].text)
+                    fs.appendFileSync('messages.txt',toString(jsonParsed.chats.list[numChat].messages[i].text), function (err){
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log("cemf");
+                        }
+                    
+                })
+        }
+        }else if(jsonParsed.chats.list[numChat].messages[i].text == 'https://youtu.be/8QWrDxXFIc8'){
+            console.log("dd")
         }
     }
+}
 
 )
